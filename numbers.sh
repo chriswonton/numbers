d1f69307e4b725d3e256afa463e5cbd22187e0b4
Christian Ton-That <69617940+chriswonton@users.noreply.github.com>
2020-12-03 23:34:42 -0800

    Add files via upload

---
+++ numbers.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# numbers.sh
+# Christian Ton-That
+echo "Please enter a positive integer:"
+read -r num
+n=1
+while [ "$n" -le "$num" ]
+if [ $n -eq 0
+then
+	echo "$n even"
+else
+	echo "$n odd"
+fi
